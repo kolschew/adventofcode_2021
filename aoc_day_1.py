c088d2795b11d30a55e45f76fa8f6b5c8a9daf6e
Kevin Olschewsky <kevin.olschewsky@gmail.com>
2021-12-01 22:21:42 +0100

    Initial commit

---
+++ aoc_day_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Import the puzzle data as numpy array #
+df1 = np.loadtxt('input_puzzles/day_1.txt')
+
+
+# Function for counting the number of increases to previous value #
+def count_increase(data):
+    prev_val = data[0]
+    cnt = 0
+
+    for curr_val in data:
+        if prev_val < curr_val:
+            cnt += 1
+        prev_val = curr_val
+    return cnt
+
+
+# Function for counting the number of increases of consecutive sums if three #
+def count_sum_increase(data):
+    prev_sum = data[0] + data[1] + data[2]
+    cnt = 0
+
+    for idx in range(1, len(data)):
+        if idx + 2 == len(data):
+            break
+        else:
+            curr_sum = data[idx] + data[idx + 1] + data[idx + 2]
+            if prev_sum < curr_sum:
+                cnt += 1
+            prev_sum = curr_sum
+    return cnt
