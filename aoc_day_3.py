d823d3feffc1c458cb47c992cf6269ac9c72c8ad
kolschew <olschewsky@physik.uni-siegen.de>
2021-12-03 15:49:37 +0100

    Corrected a typo.

--- aoc_day_3.py
+++ aoc_day_3.py
@@ -19,7 +19,7 @@ class BinaryCounter:
         self.line_len = len(data[0])
 
     @staticmethod
-    def max_occurences(arr, pos):
+    def max_occurrences(arr, pos):
         cnt = Counter(arr.str[pos])
         if cnt.most_common(2)[0][1] == cnt.most_common(2)[1][1]:
             occ = '1'
@@ -28,7 +28,7 @@ class BinaryCounter:
         return occ
 
     @staticmethod
-    def min_occurences(arr, pos):
+    def min_occurrences(arr, pos):
         cnt = Counter(arr.str[pos])
         if cnt.most_common(2)[0][1] == cnt.most_common(2)[1][1]:
             occ = '0'
@@ -39,13 +39,13 @@ class BinaryCounter:
     def gamma_rate(self):
         fin = ''
         for pos in range(self.line_len):
-            fin += self.max_occurences(self.data, pos)
+            fin += self.max_occurrences(self.data, pos)
         return int(fin, 2)
 
     def epsilon_rate(self):
         fin = ''
         for pos in range(self.line_len):
-            fin += self.min_occurences(self.data, pos)
+            fin += self.min_occurrences(self.data, pos)
         return int(fin, 2)
 
     def oxygen_rate(self):
@@ -54,7 +54,7 @@ class BinaryCounter:
             if len(ox_rate) == 1:
                 break
             else:
-                ox_rate = pd.DataFrame([i for i in ox_rate if i[pos] == self.max_occurences(ox_rate, pos)])[0]
+                ox_rate = pd.DataFrame([i for i in ox_rate if i[pos] == self.max_occurrences(ox_rate, pos)])[0]
         return int(ox_rate[0], 2)
 
     def co2_rate(self):
@@ -63,7 +63,7 @@ class BinaryCounter:
             if len(co2_rate) == 1:
                 break
             else:
-                co2_rate = pd.DataFrame([i for i in co2_rate if i[pos] == self.min_occurences(co2_rate, pos)])[0]
+                co2_rate = pd.DataFrame([i for i in co2_rate if i[pos] == self.min_occurrences(co2_rate, pos)])[0]
         return int(co2_rate[0], 2)
 
 
