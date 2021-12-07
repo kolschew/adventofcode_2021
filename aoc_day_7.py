f538dc474821eabb73bbc0eff3c2f6ec655b6336
kolschew <olschewsky@physik.uni-siegen.de>
2021-12-07 13:55:23 +0100

    Solution of day 7.

--- aoc_day_7.py
+++ aoc_day_7.py
@@ -1,6 +1,8 @@
 """
 This problem absolutely cries for numpy. Mathematically the position which has the least linear distance
-to all datapoints is simply the median.
+to all datapoints is simply the median. For the second part the best alignment position is the mean rounded down
+to the next integer, because we want to now weigh the difference accordingly. The closed form for a sum of integers is
+known thanks to 7 year old Gauss: sum(x, 0, n) = (n^2 + n) / 2.
 """
 import numpy as np
 
@@ -8,10 +10,17 @@ file = 'input_puzzles/day_7.txt'
 df7 = np.loadtxt(file, delimiter=',')
 
 
-def fuel_expanse_linear(positions):
-    med = np.median(positions)
-    fuel = np.sum(np.abs(positions - med * np.ones(len(positions))))
+def fuel_expanse_constant(positions):
+    opt_dist = np.median(positions)
+    fuel = np.sum(np.abs(positions - opt_dist))
     return fuel
 
 
-print(f'Part 1: {fuel_expanse_linear(df7)}')
+def fuel_expanse_linear(positions):
+    opt_dist = np.floor(np.mean(positions))
+    fuel = np.sum((np.abs(positions - opt_dist) ** 2 + np.abs(positions - opt_dist)) / 2)
+    return fuel
+
+
+print(f'Part 1: {fuel_expanse_constant(df7)}')
+print(f'Part 2: {fuel_expanse_linear(df7)}')
