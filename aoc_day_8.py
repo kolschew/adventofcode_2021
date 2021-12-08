e11da858c2069c444b434ac7daa362c8c387f92d
Kevin Olschewsky <kevin.olschewsky@gmail.com>
2021-12-08 23:04:41 +0100

    Solution for day 8.

--- aoc_day_8.py
+++ aoc_day_8.py
@@ -1,3 +1,9 @@
+"""
+I used a dataclass for this task today just because I wanted to finally try it out. Also I was curious if it helps
+to write a classmethod that parses the input and then a bunch of staticmethod to do the actual work on things.
+Turns out this actually makes the code very readable and tidied up, so I might come back to this in the future.
+There was no real necissity for the dataclass in the end however - anyways I still liked it.
+"""
 from dataclasses import dataclass
 from collections import defaultdict
 
@@ -23,10 +29,17 @@ class SubmarineInterface:
                 cnt += 1
         return cnt
 
+    def decode_outputs(self, inputs: str, outputs: str):
+        result = ''
+        decode = self.decode_wires(inputs)
+        for out in outputs.split(' '):
+            result += str(list(decode.keys())[list(decode.values()).index(set(out))])
+        return int(result)
+
     @staticmethod
-    def determine_wires(line):
+    def decode_wires(line: str):
         wires = defaultdict(set)
-        while len(wires) < 10:
+        while len(wires) < 10 or set() in wires.values():
             for char in line.split(' '):
                 if len(char) == 6 and wires[1].issubset(set(char)) and not wires[4].issubset(set(char)):
                     wires[0] = set(char)
@@ -53,10 +66,13 @@ class SubmarineInterface:
         return wires
 
 
-file = 'input_puzzles/day_8_test.txt'
+file = 'input_puzzles/day_8.txt'
 with open(file, 'r') as f:
     data = f.read().splitlines()
 
 sf = SubmarineInterface.parser(data)
+res_p1 = sf.count_unique_numbers(sf.output)
+res_p2 = sum([sf.decode_outputs(sf.input[ii], out) for ii, out in enumerate(sf.output)])
 
-print(f'Part 1: {sf.count_unique_numbers(sf.output)}')
+print(f'Part 1: {res_p1}')
+print(f'Part 2: {res_p2}')
